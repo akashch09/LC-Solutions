ee7962fb0284fc9dc546449624e0d81e55f977bf
Akash Chaudhary <122875858+akashch09@users.noreply.github.com>
2026-03-29 09:43:20 +0530

    Update powerOfTwo.cpp

--- powerOfTwo.cpp
+++ powerOfTwo.cpp
@@ -19,3 +19,21 @@ public:
 
     }
 };
+
+
+
+Optimized solution------------------------------------
+
+
+// class Solution {
+// public:
+//     bool isPowerOfTwo(int n) {
+//         if (n < 1) return false;
+
+//         while (n % 2 == 0) {
+//             n /= 2;
+//         }
+
+//         return n == 1;
+//     }
+// };
