41a1eed712a9018f43aa99de2a037f7158249a8c
Akash Chaudhary <122875858+akashch09@users.noreply.github.com>
2026-04-02 22:56:12 +0530

    #268. Missing Number
    LeetCode: Missing Number
    Approach: Sum formula (Expected - Actual)
    Time Complexity: O(n)
    Space Complexity: O(1)

---
+++ missingNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n * (n + 1) / 2;
+        
+        int actual = 0;
+        for(int x : nums) actual += x;
+        
+        return sum - actual;
+    }
+};
