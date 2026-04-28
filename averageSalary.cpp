41dd5f55068fe71727a8a8828cf1c5d4ef00f58b
Akash Chaudhary <122875858+akashch09@users.noreply.github.com>
2026-04-28 21:13:42 +0530

    #1491. Avg Sal Excluding the Min & Max Sal
    Time Complexity (TC): O(nlog n)
    Space Complexity (SC): O(log n)

---
+++ averageSalary.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        int n=salary.size();
+        sort(salary.begin(),salary.end());
+        double sum=0;
+        int count=0;
+        for(int i=1;i<n-1;i++){
+            sum+=salary[i];
+            count++;
+        }
+        double avg=sum/count;
+        return avg;
+    }
+};
