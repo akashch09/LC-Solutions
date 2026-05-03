5baa3a91ae4adfbb126f141147a84901795beda9
Akash Chaudhary <122875858+akashch09@users.noreply.github.com>
2026-05-03 22:05:28 +0530

    #1679. Max Number of K-Sum Pairs
    Time Complexity: O(n logn )
    Space Complexity: O(1)

---
+++ kSumPair.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int op=0;
+        int left=0;
+        int right=nums.size()-1;
+        while(left<right){
+            int sum=nums[left]+nums[right];
+            if(sum==k){
+                op++;
+                left++;
+                right--;
+            }
+            else if(sum<k){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return op;
+    }
+};
