ab43654282c41f853ed36180d18610e9bbecb256
Akash Chaudhary <122875858+akashch09@users.noreply.github.com>
2026-05-11 22:37:27 +0530

    #771. Jewels and Stones
    Time Complexity
    O(J × S)
    J = number of jewels
    S = number of stones
    Space Complexity
    O(1)

---
+++ jewelAndStone.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(char i:jewels){
+            for(char j:stones){
+                if(i==j){
+                    count++;
+                }
+            }
+        
+        }
+        return count;
+    }
+};
